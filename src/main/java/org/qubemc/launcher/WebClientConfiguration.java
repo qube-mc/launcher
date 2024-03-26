37619ccd6ba9317844f63931ce628b904f6119d1
ilss8811 <main.ilss8811@gmail.com>
2024-03-26 08:05:26 +0700

    feat: Added Spring WebFlux starter

---
+++ src/main/java/org/qubemc/launcher/WebClientConfiguration.java
@@ -0,0 +1,13 @@
+package org.qubemc.launcher;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.http.HttpClient;
+
+@Configuration
+public class WebClientConfiguration {
+
+
+
+}
